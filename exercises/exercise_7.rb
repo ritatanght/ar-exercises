e6f779e1136096d7918cc69a4459327e7def2b37
Rita Tang <doggymint@gmail.com>
2023-10-16 18:50:30 -0400

    Add code for ex7 to output validation errors for invalid store creation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,17 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+#Your code goes here ...
+print "Enter a store name: "
+store_name = gets.chomp
+store = Store.create(name: store_name)
+
+store.errors.each do |error|
+  puts "Validation Error: #{error.full_message}"
+end
+
+# create employee without attaching to a store
+# employee1 = Employee.create(first_name: "Ruby", last_name: "Gem", hourly_rate: 80)
+# employee1.errors.each do |error|
+#   puts "Validation Error: #{error.full_message}"
+# end
