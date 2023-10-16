f7dafe2d07ba3f5d8d7f527ea77df475e605f001
Rita Tang <doggymint@gmail.com>
2023-10-16 17:11:01 -0400

    Add code for ex6 to create employees under stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Aleena", last_name: "Anderson", hourly_rate: 50)
+@store1.employees.create(first_name: "Cormac", last_name: "Whitehead", hourly_rate: 45)
+
+@store2.employees.create(first_name: "Betsy", last_name: "Mccarthy", hourly_rate: 40)
+@store2.employees.create(first_name: "Kingsley", last_name: "Woods", hourly_rate: 65)
+@store2.employees.create(first_name: "Jerry", last_name: "Hahn", hourly_rate: 100)
+
+puts "Total employees: #{Employee.count}"
