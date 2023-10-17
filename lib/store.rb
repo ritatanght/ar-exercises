24188a6d0cd6303cb2627beee3822877ab94792a
Rita Tang <doggymint@gmail.com>
2023-10-16 22:38:53 -0400

    Add restriction to disallow code destroy store with employees

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,5 @@
 class Store < ActiveRecord::Base
-  has_many :employees
+  has_many :employees, dependent: :restrict_with_error
   validates :name, length: { minimum: 5 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :at_least_one_apparel
@@ -10,4 +10,5 @@ class Store < ActiveRecord::Base
       errors.add(:womens_apparel, "must carry at least one of the men's or women's apparel") 
     end
   end
+
 end
