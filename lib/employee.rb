6583521d97b81837752dd07fb72cc8a3bde61570
Rita Tang <doggymint@gmail.com>
2023-10-16 22:00:10 -0400

    Add callback to create password before_create

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,12 @@ class Employee < ActiveRecord::Base
   validates :last_name, presence: true
   validates :hourly_rate, numericality: { only_integer: true }, inclusion: { in: 40..200 }
   validates :store_id, presence:true
+
+  before_create :create_random_password
+
+  private
+  def create_random_password
+    self.password = [*('A'..'Z'), *('a'..'z'), *('0'..'9')].sample(8).join
+  end
 end
+
