19a19acd22a2a946cfb2c76efdeb3005c00d3378
Rita Tang <doggymint@gmail.com>
2023-10-16 22:31:55 -0400

    Remove setup as it is called in the ex1

--- callbacks-exercises/exercise_1.rb
+++ callbacks-exercises/exercise_1.rb
@@ -1,4 +1,3 @@
-require_relative '../setup'
 require_relative '../exercises/exercise_1.rb'
 
 puts "Stretch Exercise 1"
@@ -6,3 +5,6 @@ puts "----------"
 @store1 = Store.find(id = 1)
 employee1 = @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 puts "Employee1's password: #{employee1.password}"
+
+
+
